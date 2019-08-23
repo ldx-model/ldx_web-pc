2487ce1991620192a4a08228a0f087ea904934e0
xuyangyang <544832866@qq.com>
2019-08-23 17:01:06 +0800

    脚手架

---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="grid-content bg-purple">1</div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">1</div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">1</div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">1</div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
